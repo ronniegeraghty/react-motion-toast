5225be64d4ce3e1baff0194b5c4656dab4362a78
Ronnie Geraghty <ronniegerag@gmail.com>
2019-10-15 11:56:51 -0400

    added scripting to have module auto rebuild and load new into test-app then restart the test app on saving to the react-motion-toast src folder

--- react-motion-toast/src/index.js
+++ react-motion-toast/src/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class MotionToast extends Component {
   render() {
-    return <div>Ronnie Test Test Test</div>;
+    return <div>Ronnie</div>;
   }
 }
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -4,5 +4,4 @@ echo COMPLETED BUILD
 cd ../test-app
 yarn add ../react-motion-toast
 echo INSTALLED NEW MODULE VERSION
-echo RESTARTING REACT APP
-yarn start
+npm run start
